43aeaaa972a92d3667e6aa2fa34e0d81b2324253
Sanchit Srivastava <30755370+sanchit247@users.noreply.github.com>
2019-03-27 17:52:52 +0530

    unit test example 2

---
+++ UnitTestExample/UnitTestExample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  UnitTestExample
+//
+//  Created by sanchit on 26/03/19.
+//  Copyright © 2019 com.sanchit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    func returnSquare(num : Int) -> Int{
+        return num * num
+    }
+
+}
+
---
+++ UnitTestExample/UnitTestExampleTests/UnitTestExampleTests.swift
@@ -0,0 +1,14 @@
+//
+//  UnitTestExampleTests.swift
+//  UnitTestExampleTests
+//
+//  Created by sanchit on 26/03/19.
+//  Copyright © 2019 com.sanchit. All rights reserved.
+//
+
+import XCTest
+
+class UnitTestExampleTests: XCTestCase {
+
+
+}
