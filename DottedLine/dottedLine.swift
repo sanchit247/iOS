2ae13fd9365ee0d6f47f1fe00809390a0015f5bd
Sanchit Srivastava <30755370+sanchit247@users.noreply.github.com>
2019-08-29 19:58:36 +0530

    This function creates a dotted line

---
+++ DottedLine/dottedLine.swift
@@ -0,0 +1,10 @@
+func drawDottedLine(start p0: CGPoint, end p1: CGPoint, view: UIView) {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.strokeColor = UIColor.lightGray.cgColor
+        shapeLayer.lineWidth = 1
+        shapeLayer.lineDashPattern = [7, 3] // 7 is the length of dash, 3 is length of the gap.
+        let path = CGMutablePath()
+        path.addLines(between: [p0, p1])
+        shapeLayer.path = path
+        view.layer.addSublayer(shapeLayer)
+}
