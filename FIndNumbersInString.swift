5e5f3d9c8144b8a9a042d06ff5206ccc1cfae7ba
Sanchit Srivastava <30755370+sanchit247@users.noreply.github.com>
2019-11-04 17:09:46 +0530

    Return numbers from given string

---
+++ FIndNumbersInString.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+//var str = "Hello, playground"
+let str = "0 sds sx 3 ss "
+func findNumbers(str:String) -> [Int]{
+    var nums = [Int]()
+    var isDigitStarted:Bool = false
+    var sum = 0
+    var counter = 0
+    for i in str{
+        
+        if let val = Int("\(i)"){
+               sum = sum * 10 + val
+               isDigitStarted = true
+               if counter == str.count - 1{
+                nums.append(sum)
+               }
+        }
+        else{
+            if isDigitStarted{
+                nums.append(sum)
+                isDigitStarted = false
+                sum = 0
+            }
+        }
+        counter += 1
+    }
+    return nums
+}
+
+print(findNumbers(str: str))
