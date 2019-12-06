07906dc47b03dee277f893828b2d3a46f8b19f78
Sanchit Srivastava <30755370+sanchit247@users.noreply.github.com>
2019-12-06 16:57:54 +0530

    Updated corner radius ui button

--- circularButtonExtensio/EXTENSION_FOR_ROUNDED_BTN.swift
+++ circularButtonExtensio/EXTENSION_FOR_ROUNDED_BTN.swift
@@ -7,3 +7,23 @@ extension UIButton{
         self.clipsToBounds = true
     }
 }
+@IBDesignable extension UIButton {
+
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+}
